7d6de4abde0db9475121bd6a973611c6b6375e0d
Ashutosh Koli <65846737+Ashutosh-koli@users.noreply.github.com>
2025-04-21 10:15:32 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,3 @@
-// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
@@ -11,7 +10,7 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
